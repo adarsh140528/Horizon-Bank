313ed46782fa21a48c7c19978142482df93408b0
Adarshgft <166240193+Adarshgft@users.noreply.github.com>
2025-12-08 22:45:21 +0530

    fg

--- backend/bank_backend_server.js
+++ backend/bank_backend_server.js
@@ -21,16 +21,16 @@ console.log("====================");
 
 const app = express();
 
-// Middleware
-app.use(
-    cors({
-      origin: "*",
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-      allowedHeaders: ["Content-Type", "Authorization"],
-    })
-  );
+app.use(cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type,Authorization",
+  }));
   
-app.use(express.json());
+  app.options("*", cors()); 
+  
+  // Needed for JSON body parsing
+  app.use(express.json());
 
 // DB
 connectDB();
