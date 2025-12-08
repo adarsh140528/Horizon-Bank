aa1a60b3c31f45aaba00a5e1e8da2ea44b6f383a
Adarshgft <166240193+Adarshgft@users.noreply.github.com>
2025-12-08 21:39:37 +0530

    link

--- backend/bank_backend_server.js
+++ backend/bank_backend_server.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(cors({
     origin: [
       "http://localhost:5173",
-      "horizon-bank-jtv5pcrnb-adarshhs-projects.vercel.app"
+      "horizon-bank-lyrshezxz-adarshhs-projects.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
